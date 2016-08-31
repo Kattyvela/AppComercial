242cbc7ba61b8dfc4caed5b187d25622e133f6af
Kattyvela <ktty_vv888@hotmail.com>
2016-08-31 16:17:31 -0500

    creacion del proyecto

---
+++ app/src/main/java/appcomercial/parquesanto/com/ec/appcomercial/Inicio.java
@@ -0,0 +1,13 @@
+package appcomercial.parquesanto.com.ec.appcomercial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
